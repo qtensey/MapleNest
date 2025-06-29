86d7250f2b5c7782f7aea98b1acfb66929b02d14
tensey <tensey26@gmail.com>
2025-06-29 19:40:53 +0300

    push-final

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,38 @@
 # routes.py
-from flask import Blueprint, render_template, request
-from .models import db, Order
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from app.utils import send_email
+
 
 main = Blueprint('main', __name__)
+
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
+
 @main.route('/order', methods=['POST'])
-def order():
+def handle_order():
+    square = request.form['square']
+    service = request.form['service']
     name = request.form['name']
     phone = request.form['phone']
-    service = request.form['service']
-    date = request.form['date']
-    new_order = Order(name=name, phone=phone, service=service, date=date)
-    db.session.add(new_order)
-    db.session.commit()
-    return 'Спасибо! Мы с вами свяжемся.'
+
+    message = f"""Нова заявка з сайта:
+Ім'я: {name}
+Телефон: {phone}
+Послуга: {service}
+Площа: {square} м²"""
+
+    try:
+        send_email("Новая заявка", message)
+        flash("Заявка успешно отправлена!", "success")
+    except Exception as e:
+        print(f"Ошибка при отправке: {e}")
+        flash("Ошибка при отправке заявки.", "danger")
+
+    return redirect(url_for('main.index'))
+
 
 @main.route('/services/<service_name>')
 def service(service_name):
---
+++ app/utils.py
@@ -0,0 +1,20 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+
+def send_email(subject, message):
+    msg = MIMEText(message)
+    msg["Subject"] = subject
+    msg["From"] = EMAIL
+    msg["To"] = EMAIL
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL, PASSWORD)
+        server.send_message(msg)
