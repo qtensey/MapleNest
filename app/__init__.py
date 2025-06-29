7c8b731d5ff0781912b379bb6687189131706b29
tensey <tensey26@gmail.com>
2025-06-29 20:49:44 +0300

    site_host_commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
-from .models import db
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     app.config['SECRET_KEY'] = 'секрет'
-
-    db.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
